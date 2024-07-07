e4f85b5ecd279fbb107ba2dc035730f06df68e8c
Murat Toprak <murattoprak2004@gmail.com>
2024-07-07 19:32:43 +0300

    feat: add syntax highlightings for strings, comments and special keywords

--- textoprak.c
+++ textoprak.c
@@ -19,9 +19,9 @@
 
 /* defines */
 
-#define KILO_VERSION "0.0.1"
-#define KILO_TAB_STOP 8
-#define KILO_QUIT_TIMES 3
+#define TEXTOPRAK_VERSION "0.0.1"
+#define TEXTOPRAK_TAB_STOP 8
+#define TEXTOPRAK_QUIT_TIMES 3
 #define DEFAULT_BUFFER_SIZE 80
 
 #define CTRL_KEY(k) ((k) & 0x1f) 
@@ -41,26 +41,38 @@ enum editorKey {
 
 enum editorHighlight {
 	HL_NORMAL = 0,
+	HL_COMMENT,
+	HL_MLCOMMENT,
+	HL_KEYWORD1,
+	HL_KEYWORD2,
+	HL_STRING,
 	HL_NUMBER,
 	HL_MATCH
 };
 
 #define HL_HIGHLIGHT_NUMBERS (1<<0)
+#define HL_HIGHLIGHT_STRINGS (1<<1)
 
 /* data */
 
 struct editorSyntax {
 	char *filetype;
 	char **filematch;
+	char **keywords;
+	char *singleline_comment_start;
+	char *multiline_comment_start;
+	char *multiline_comment_end;
 	int flags;
 };
 
 typedef struct erow {
+	int idx;
 	int size;
 	int rsize;
 	char *chars;
 	char *render;
 	unsigned char *hl;
+	int hl_open_comment;
 } erow;
 
 struct editorConfig {
@@ -87,12 +99,23 @@ struct editorConfig E;
 
 char *C_HL_extensions[] = { ".c", ".h", ".cpp", NULL};
 
+// keywords that end with the character '|' are secondary others are primary
+char *C_HL_keywords[] = {
+  "switch", "if", "while", "for", "break", "continue", "return", "else",
+  "struct", "union", "typedef", "static", "enum", "class", "case",
+  
+  "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
+  "void|", NULL
+};
+
 // highlight database
 struct editorSyntax HLDB[] = {
 	{
 		"c",
 		C_HL_extensions,
-		HL_HIGHLIGHT_NUMBERS
+		C_HL_keywords,
+		"//", "/*", "*/",
+		HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
 	},
 };
 
@@ -242,14 +265,77 @@ void editorUpdateSyntax(erow *row) {
 
 	if (E.syntax == NULL) return;
 
+	char **keywords = E.syntax->keywords;
+
+	char *scs = E.syntax->singleline_comment_start;
+	char *mcs = E.syntax->multiline_comment_start;
+	char *mce = E.syntax->multiline_comment_end;
+
+	int scs_len = scs ? strlen(scs) : 0;
+	int mcs_len = mcs ? strlen(mcs) : 0;
+	int mce_len = mce ? strlen(mce) : 0;
+
 	int prev_sep = 1;
+	int in_string = 0;
+	int in_comment = (row->idx > 0 && E.row[row->idx -1].hl_open_comment);
 
 	int i = 0;
 	while (i < row->rsize) {
 		char c = row->render[i];
 		unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+		
+		if (scs_len && !in_string && !in_comment) {
+			if (!strncmp(&row->render[i], scs, scs_len)) {
+				memset(&row->hl[i], HL_COMMENT, row->rsize - i);
+				break;
+			}
+		}
+
+		if (mcs_len && mce_len && !in_string) {
+			if (in_comment) {
+				row->hl[i] = HL_MLCOMMENT;
+				if (!strncmp(&row->render[i], mce, mce_len)) {
+					memset(&row->hl[i], HL_MLCOMMENT, mce_len);
+					i+= mce_len;
+					in_comment = 0;
+					prev_sep = 1;
+					continue;
+				} else {
+					i++;
+					continue;
+				}
+			} else if (!strncmp(&row->render[i], mcs, mcs_len)) {
+				memset(&row->hl[i], HL_MLCOMMENT, mcs_len);
+				i += mcs_len;
+				in_comment = 1;
+				continue;
+			}
+		}
+
+		if (E.syntax->flags & HL_HIGHLIGHT_STRINGS) {
+			if (in_string) {
+				row->hl[i] = HL_STRING;
+				if (c == '\\' && i + 1 < row->rsize) {
+					row->hl[i + 1] = HL_STRING;
+					i += 2;
+					continue;
+				}
+				if (c == in_string) in_string = 0;
+				i++;
+				prev_sep = 1;
+				continue;
+			} else {
+				if (c == '"' || c == '\'') {
+					in_string = c;
+					row->hl[i] = HL_STRING;
+					i++;
+					continue;
+				}
+			}
+		}
+
 		if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS) {
-			if (isdigit(c) && (prev_sep || prev_hl == HL_NUMBER) || 
+			if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) || 
 				(c == '.' && prev_hl == HL_NUMBER)) {
 				row->hl[i] = HL_NUMBER;
 				i++;
@@ -257,13 +343,46 @@ void editorUpdateSyntax(erow *row) {
 				continue;
 			}
 		}
+
+		if (prev_sep) {
+			int j;
+			for (j = 0; keywords[j]; j++) {
+				// Checking primary or secondary keyword
+				int klen = strlen(keywords[j]);
+				int kw2 = keywords[j][klen - 1] == '|';
+				if (kw2) klen--;
+
+				if (!strncmp(&row->render[i], keywords[j], klen) &&
+					is_separator(row->render[i + klen])) {
+					memset(&row->hl[i], kw2 ? HL_KEYWORD2 : HL_KEYWORD1, klen);
+					i+= klen;
+					break;
+				}
+			}
+			if (keywords[j] != NULL) {
+				prev_sep = 0;
+				continue;
+			}
+		}
+
 		prev_sep = is_separator(c);
 		i++;
+	}
+
+	int changed = (row->hl_open_comment != in_comment);
+	row->hl_open_comment = in_comment;
+	if (changed && row->idx + 1 < E.numrows) {
+		editorUpdateSyntax(&E.row[row->idx + 1]);
 	}
 }
 
 int editorSyntaxToColor(int hl) {
 	switch(hl) {
+		case HL_COMMENT: 
+		case HL_MLCOMMENT: return 36;
+		case HL_KEYWORD1: return 33;
+		case HL_KEYWORD2: return 32;
+		case HL_STRING: return 35;
 		case HL_NUMBER: return 31;
 		case HL_MATCH: return 34;
 		default: return 37;
@@ -303,7 +422,7 @@ int editorRowCxToRx(erow *row, int cx) {
 	int j;
 	for (j = 0; j < cx; j++) {
 		if (row->chars[j] == '\t')
-			rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
+			rx += (TEXTOPRAK_TAB_STOP - 1) - (rx % TEXTOPRAK_TAB_STOP);
 		rx++;
 	}
 	return rx;
@@ -314,7 +433,7 @@ int editorRowRxToCx(erow *row, int rx) {
 	int cx;
 	for (cx = 0; cx < row->size; cx++) {
 		if (row->chars[cx] == '\t')
-			cur_rx += (KILO_TAB_STOP - 1) - (cur_rx % KILO_TAB_STOP);
+			cur_rx += (TEXTOPRAK_TAB_STOP - 1) - (cur_rx % TEXTOPRAK_TAB_STOP);
 		cur_rx++;
 
 		if (cur_rx > rx) return cx;
@@ -329,14 +448,14 @@ void editorUpdateRow(erow *row) {
 		if (row->chars[j] == '\t') tabs++;
 
 	free(row->render);
-	row->render = malloc(row->size + tabs*(KILO_TAB_STOP - 1) + 1);
+	row->render = malloc(row->size + tabs*(TEXTOPRAK_TAB_STOP - 1) + 1);
 
 	// Copy the content of row to render
 	int idx = 0;
 	for (j = 0; j < row->size; j++) {
 		if (row->chars[j] == '\t') {
 			row->render[idx++] = ' ';
-			while (idx % KILO_TAB_STOP != 0) row->render[idx++] = ' ';
+			while (idx % TEXTOPRAK_TAB_STOP != 0) row->render[idx++] = ' ';
 		} else {
 		row->render[idx++] = row->chars[j];
 		}
@@ -352,6 +471,10 @@ void editorInsertRow(int at, char *s, size_t len) {
 	
 	E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
 	memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+	for (int j = at + 1; j <= E.numrows; j++) E.row[j].idx++;
+
+	E.row[at].idx = at;
+
 	E.row[at].size = len;
 	E.row[at].chars = malloc(len + 1);
 	memcpy(E.row[at].chars, s, len);
@@ -360,6 +483,7 @@ void editorInsertRow(int at, char *s, size_t len) {
 	E.row[at].rsize = 0;
 	E.row[at].render = NULL;
 	E.row[at].hl = NULL;
+	E.row[at].hl_open_comment = 0;
 	editorUpdateRow(&E.row[at]);
 
 	E.numrows++;
@@ -376,6 +500,7 @@ void editorDelRow(int at) {
 	if (at < 0 || at >= E.numrows) return;
 	editorFreeRow(&E.row[at]);
 	memmove(&E.row[at], &E.row[at + 1], sizeof(erow) * (E.numrows - at - 1));
+	for (int j = at; j < E.numrows - 1; j++) E.row[j].idx--;
 	E.numrows--;
 	E.dirty++;
 }
@@ -660,7 +785,7 @@ void editorDrawRows(struct abuf *ab) {
 			if (E.numrows == 0 && y == E.screenrows / 3) {
 				char welcome[DEFAULT_BUFFER_SIZE];
 				int welcomelen = snprintf(welcome, sizeof(welcome),
-					"Kilo editor -- version %s", KILO_VERSION);
+					"TEXTOPRAK editor -- version %s", TEXTOPRAK_VERSION);
 				if (welcomelen > E.screencols)
 					welcomelen = E.screencols;
 
@@ -686,7 +811,19 @@ void editorDrawRows(struct abuf *ab) {
 			int current_color = -1;
 			int j;
 			for (j = 0; j < len; j++) {
-				if (hl[j] == HL_NORMAL) {
+				if (iscntrl(c[j])) {
+					// Convert control characters to uppercase letters by adding '@' to their value
+					char sym = (c[j] <= 26) ? '@' + c[j] : '?';
+					// Invert colors
+					abAppend(ab, "\x1b[7m", 4);
+					abAppend(ab, &sym, 1);
+					abAppend(ab, "\x1b[m", 3);
+					if (current_color != -1) {
+						char buf[16];
+						int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", current_color);
+						abAppend(ab, buf, clen);
+					}
+				} else if (hl[j] == HL_NORMAL) {
 					if (current_color != -1) {
 						abAppend(ab, "\x1b[39m", 5);
 						current_color = -1;
@@ -931,7 +1068,7 @@ void editorMoveCursor(int key) {
 }
 
 void editorProcessKeypress(void) {
-	static int quit_times = KILO_QUIT_TIMES;
+	static int quit_times = TEXTOPRAK_QUIT_TIMES;
 
 	int c = editorReadKey();
 
@@ -1007,7 +1144,7 @@ void editorProcessKeypress(void) {
 			break;
 	}
 
-	quit_times = KILO_QUIT_TIMES;
+	quit_times = TEXTOPRAK_QUIT_TIMES;
 }
 
 /* init */
